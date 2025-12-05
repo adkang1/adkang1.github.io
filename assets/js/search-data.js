58a16628991f1615cf2f84fe4c8889ac9f9b30bd
adkang1 <52758080+adkang1@users.noreply.github.com>
2025-12-05 04:55:35 +0000

    Deploying to gh-pages from @ adkang1/adkang1.github.io@c25f15cd2e84ad3cf75c5ec579a711aef7f19ddc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -488,7 +488,7 @@ ninja.data = [{
         title: 'Inspire HEP',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://inspirehep.net/authors/", "_blank");
         },
       },{
         id: 'social-linkedin',
@@ -510,13 +510,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=zBy_7xAAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
