77995b6b64017cf48db0f9706119f7e16219d3a6
adkang1 <52758080+adkang1@users.noreply.github.com>
2025-12-05 01:15:24 +0000

    Deploying to gh-pages from @ adkang1/adkang1.github.io@e6f4087e5d2637f960a08331b071849dfe7e07d0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,14 +501,21 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/resume.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%64%6B%61%6E%67@%67.%75%63%6C%61.%65%64%75", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/adkang1", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -516,6 +523,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/andrea-kang-b1b659143", "_blank");
         },
       },{
         id: 'social-rss',
@@ -529,7 +543,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=zBy_7xAAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
