06177f0ed0cf883827746c486ac7eca2c13fca7b
adkang1 <52758080+adkang1@users.noreply.github.com>
2025-12-05 04:22:12 +0000

    Deploying to gh-pages from @ adkang1/adkang1.github.io@3acbb1cc8b31866d48a3193133354c6c0979adf1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,18 +440,12 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+            },},{id: "news-excited-to-present-our-paper-quot-synthetic-data-generation-with-large-language-models-for-improved-depression-prediction-quot-at-cogsci-2025",
+          title: 'Excited to present our paper &amp;quot;Synthetic Data Generation with Large Language Models for...',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-project-1",
+              window.location.href = "/news/announcement_1/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
